0ae8dfd8d0a9346aa37fcaaa0bbdc0e4420b05c8
Ethan Evans <ethanalexevans@gmail.com>
2025-03-06 22:04:13 -0800

    initial

---
+++ Info.lua
@@ -0,0 +1,14 @@
+return {
+    VERSION = { major = 0, minor = 1, revision = 0, },
+    
+    LrSdkVersion        = 14.0,
+    LrSdkMinimumVersion = 14.0,
+    
+    LrToolkitIdentifier = "com.ethan.desqueeze",
+    LrPluginName        = "Desqueeze",
+
+    LrLibraryMenuItems = {
+        title = "Desqueeze",
+        file  = "desqueeze.lua",
+    },
+}
